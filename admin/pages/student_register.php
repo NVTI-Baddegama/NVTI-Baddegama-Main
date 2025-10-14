e6623f1dcc8bf8a6b93282ebd33b88d93120cddc
CHAMI-csr <chamikasandeepa40@gmail.com>
2025-10-15 00:20:22 +0530

    new

--- admin/pages/student_register.php
+++ admin/pages/student_register.php
@@ -84,7 +84,7 @@
                     </div>
                 </div>
 
-                <div class="p-4 border border-gray-200 rounded-lg bg-gray-50">
+                <div id="olSubjectsContainer" class="p-4 border border-gray-200 rounded-lg bg-gray-50 hidden">
                     <p class="text-sm font-medium text-gray-700 mb-3">If passed O/L, please provide grades for key subjects:</p>
                     <div class="grid grid-cols-3 gap-4">
                         <div>
@@ -178,6 +178,9 @@
         </form>
     </div>
 </div>
+
+<script src="../js/passcheck.js"></script>
+
 
 
 <?php include_once ('../include/footer.php'); ?>
