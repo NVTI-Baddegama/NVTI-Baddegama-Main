a4872fa3a75568d70db50b60f4786f93a12dfc54
chiki4984234 <infor.chamika@gmail.com>
2025-10-28 12:24:21 +0530

    detabase_change

--- include/connection.php
+++ include/connection.php
@@ -1,24 +1,24 @@
 <?php
-$servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$dbname = "nvti_baddegama"; 
+// $servername = "localhost";
+// $username = "root"; 
+// $password = ""; 
+// $dbname = "nvti_baddegama"; 
+
+// $con = new mysqli($servername, $username, $password, $dbname);
+
+// if ($con->connect_error) {
+//     die("Connection failed: " . $con->connect_error);
+// }
+?>
+<?php
+$servername = "nvtibaddegama.site";
+$username = "nvtibadde"; 
+$password = "3CY+C9*etd9Qz9"; 
+$dbname = "nvtibadde_vta_baddegama"; 
 
 $con = new mysqli($servername, $username, $password, $dbname);
 
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
-?>
-<?php
-// $servername = "sql200.alwaysfreehost.xyz";
-// $username = "xeyra_40108320"; 
-// $password = "mAMwYnllh0hmsV4"; 
-// $dbname = "xeyra_40108320_Chiki"; 
-
-// $con = new mysqli($servername, $username, $password, $dbname);
-
-// if ($con->connect_error) {
-//     die("Connection failed: {$con->connect_error}");
-// }
 ?>
